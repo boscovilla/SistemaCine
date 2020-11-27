6bcf39509f3b2be39eec7f395d605d3436a78eb4
Axel0077 <axelleonardoperezm@gmail.com>
2020-11-26 21:32:54 -0600

    Agregando configuracion para poder consumir el api desde una aplicacion cliente

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,19 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistencia;
-
-
 
 namespace WebAPI
 {
@@ -29,6 +20,7 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddDbContext<SistemaCineContext>
                 (opt =>
                 {
@@ -41,6 +33,15 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var baseUrl = $"http://localhost:3000"; // URL base de la aplicacion cliente
+
+            app.UseCors(options =>
+            {
+                options.WithOrigins(baseUrl);
+                options.AllowAnyMethod();
+                options.AllowAnyHeader();
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
